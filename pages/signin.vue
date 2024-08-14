d13b0ba82c5180bbada1abbd719bf5c5cf479e28
asif512 <asifh2591@gmail.com>
2024-08-14 13:52:53 +0500

    signup form ui changes

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="signin min-h-screen  text-gray-800  px-4 py-6 flex flex-col justify-center sm:py-12">
-        <div class="relative py-3 w-full max-w-[500px] mx-auto text-center">
-            <div class="relative mt-4 bg-white shadow-md sm:rounded-lg text-left">
-                <div class="h-2 bg-indigo-400 rounded-t-md"></div>
-                <div class="py-6 px-8">
-                    <label class="block font-semibold">Username</label>
+    <div class="signin min-h-screen  text-gray-800  px-4 py-6 flex flex-col justify-center">
+        <div class="relative py-3 w-full max-w-[800px] mx-auto text-center">
+            <div class="mt-4 bg-white shadow-md rounded-xl flex overflow-hidden">
+                <div class="h-full min-h-full w-1/2">
+                    <img class="h-[386px]" src="/public/images/4.jpg" alt="">
+                </div>
+                <div class="py-6 px-8 text-left w-1/2">
+                    <label class="block font-bold text-base uppercase">login form</label>
+                    <label class="block font-semibold mt-5">Username:</label>
                     <input type="text" placeholder="username" v-model="username"
                         class=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md">
-                    <label class="block mt-3 font-semibold">Password</label>
+                    <label class="block mt-3 font-semibold">Password:</label>
                     <input type="password" placeholder="password" v-model="password"
                         class=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md">
                     <div class="flex mt-5 justify-between items-baseline">
@@ -16,8 +19,8 @@
                             Login
                         </button>
                     </div>
-                    <div class="uppercase text-center my-4">or</div>
-                    <div class="flex mt-5 justify-between items-baseline">
+                    <div class="uppercase text-center my-2">or</div>
+                    <div class="flex justify-between items-baseline">
                         <NuxtLink class="w-full"  to="signup">
                             <button type="button"
                                 class="rounded-md w-full uppercase bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
