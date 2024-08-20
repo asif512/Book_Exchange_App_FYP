78196f1f4f78486606d7dc175a68e368307de9bb
asif512 <asifh2591@gmail.com>
2024-08-20 20:24:08 +0500

    about and contact page.

--- pages/About.vue
+++ pages/About.vue
@@ -1,14 +1,41 @@
 <template>
-    <div class="about-page">
-      <Header></Header>
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pb-10">
-       About page
-      </div>
+  <div class="about-page">
+    <Header></Header>
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pb-10 pt-6">
+      <h2 class="font-bold uppercase text-2xl mb-4">About us</h2>
+      <h2 class="font-bold uppercase mt-2">Welcome to Book Exchange App!</h2>
+      <p class="mt-2">At Book Exchange App, we're passionate about connecting book lovers and fostering a
+        community where you can exchange, buy, and sell books with ease. Our platform allows you to
+        post books you're looking to exchange or sell and discover new reads from fellow book enthusiasts.</p>
+      <h2 class="uppercase font-bold mt-6">Our Mission</h2>
+      <p class="mt-2">Our mission is to make book swapping and trading accessible and enjoyable for everyone. We believe
+        in the power of reading to enrich lives and bring people together, and we strive to create a space where
+        literature can be shared and enjoyed by all..</p>
+      <h2 class="uppercase font-bold mt-6">What We Offer</h2>
+      <ul class="mt-2">
+        <li><span class="font-bold">Easy Book Listings:</span> Post your books with just a few details – title,
+          description, category, and image.</li>
+        <li> <span class="font-bold">Seamless Exchange:</span> Connect with other users to arrange book trades or
+          purchases.</li>
+        <li> <span class="font-bold">Diverse Categories:</span> Explore a wide range of book categories to find your
+          next great read.</li>
+        <li> <span class="font-bold">User-Friendly Interface:</span> Enjoy a simple and intuitive experience on both
+          mobile and web platforms.</li>
+      </ul>
+      <h2 class="uppercase font-bold mt-6">Our Team</h2>
+      <p class="mt-2">We are a dedicated team of book lovers and tech enthusiasts committed to building a platform that
+        enhances the book-sharing experience. If you have any questions or feedback, we'd love to hear from you!</p>
+
+      <h2 class="uppercase font-bold mt-6">Join Us</h2>
+      <p class="mt-2">Start exploring today and become part of our growing community of book lovers. Whether you're
+        looking to find new books or share your collection with others, Book Exchange App is here to help.</p>
+
     </div>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped lang="scss"></style>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss"></style>
--- pages/Contact.vue
+++ pages/Contact.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="contact-page">
-      <Header></Header>
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pb-10">
-       Contact page
-      </div>
+  <div class="contact-page">
+    <Header></Header>
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 pb-10 pt-6">
+      <h2 class="font-bold uppercase text-2xl mb-4">Contact us</h2>
+      <h2 class="font-bold uppercase mt-2">We’re Here to Help!</h2>
+      <p class="mt-2">Have questions, feedback, or need assistance? Feel free to reach out to us through any of the
+        following methods:</p>
+
+      <h2 class=" font-bold mt-4 flex items-center">Contact: <p class="font-medium ml-4">booksexchangeapp@gmail.com</p>
+      </h2>
+      <h2 class=" font-bold mt-4 flex items-center">Phone: <p class="font-medium ml-4">111-222-333</p>
+      </h2>
+      <h2 class=" font-bold mt-4 flex items-center">Address: <p class="font-medium ml-4">Book Exchange App
+          123 Book Lane
+          ABC City, LC 45678
+          XYX
+        </p>
+      </h2>
+
+      <h2 class="uppercase font-bold mt-6">Business Hours:</h2>
+      <p class="mt-2">
+        Monday - Friday: 9:00 AM - 6:00 PM
+        <br>
+        Saturday - Sunday: Closed
+      </p>
     </div>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped lang="scss"></style>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss"></style>
